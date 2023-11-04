ef16664e84cf7e569e1b5bf592a8da834b2795e9
haichuan <haichuan@fudata.cn>
2023-11-05 03:42:21 +0800

    zero

--- src/zero.cpp
+++ src/zero.cpp
@@ -1,3 +1,24 @@
+#include "cmdline.hpp"
 #include "fmt/core.h"
 
-int main() {}
+#include <string>
+
+using namespace zero;
+
+void usage() {
+    fmt::println("./zero [--help] [--verbose]");
+    fmt::println("options:");
+    fmt::println("    --help         print usage");
+    fmt::println("    --verbose      verbose message");
+}
+
+int main(int argc, char *argv[]) {
+    bool verbose{};
+    CmdLine::BoolOpt(&verbose, "verbose");
+    CmdLine::SetUsage(usage);
+    int res = CmdLine::Parse(argc, argv);
+
+    if (res == 0) {
+        // ...
+    }
+}
