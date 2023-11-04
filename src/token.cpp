90fbad505e8cce9d819bde54d859795f0932a255
haichuan <haichuan@fudata.cn>
2023-11-05 07:03:03 +0800

    fix

--- src/token.cpp
+++ src/token.cpp
@@ -1,18 +1,22 @@
 #include "token.hpp"
 
+#include "fmt/core.h"
+
 namespace zero {
 std::string Token::to_string() const {
     std::string literal_str;
 
     switch (type) {
         case (token_type::IDENTIFIER):
-            literal_str = lexeme;
+            literal_str = "id";
             break;
         case (token_type::STRING):
-            literal_str = std::any_cast<std::string>(literal);
+            // literal_str = std::any_cast<std::string>(literal);
+            literal_str = "string";
             break;
         case (token_type::NUMBER):
-            literal_str = std::to_string(std::any_cast<int>(literal));
+            // literal_str = std::to_string(std::any_cast<int>(literal));
+            literal_str = "number";
             break;
         case (token_type::TRUE):
             literal_str = "true";
@@ -24,7 +28,6 @@ std::string Token::to_string() const {
             literal_str = "nil";
     }
 
-    return std::to_string(static_cast<int>(type)) + " " + lexeme + " "
-           + literal_str;
+    return fmt::format("{} {}", literal_str, lexeme);
 }
 } // namespace zero
