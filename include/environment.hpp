c9664c3fd7442783eab9069360a21e95ea76dfcb
uchenily <uchenily@qq.com>
2023-11-06 02:08:13 +0000

    update include/environment.hpp
    Signed-off-by: uchenily <uchenily@qq.com>

--- include/environment.hpp
+++ include/environment.hpp
@@ -10,19 +10,25 @@ namespace zero {
 class Environment {
 
 public:
-    Environment() : enclosing(nullptr){};
+    Environment() = default;
     explicit Environment(std::unique_ptr<Environment> enclosing)
         : enclosing(std::move(enclosing)){};
+
+public:
+    // 获取一个环境变量        
     std::any get(const Token &name);
+    // 给环境变量赋值
     void assign(const Token &name, std::any value);
+    // 在当前环境定义一个变量
     void define(const std::string &name, std::any value);
 
+    // 外层环境
     std::unique_ptr<Environment> get_enclosing() {
         return std::move(this->enclosing);
     }
 
 private:
-    std::unique_ptr<Environment> enclosing;
-    std::map<std::string, std::any> values;
+    std::unique_ptr<Environment> enclosing{}; // 外层的封闭环境
+    std::map<std::string, std::any> values; // 根据token的词位信息存储变量值
 };
 } // namespace zero
