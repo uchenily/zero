f68ea7567c4ebcbb4818b9ff108668ebb57284b0
uchenily <uchenily@qq.com>
2023-11-06 01:49:01 +0000

    update include/parser.hpp
    Signed-off-by: uchenily <uchenily@qq.com>

--- include/parser.hpp
+++ include/parser.hpp
@@ -9,19 +9,29 @@
 #include <vector>
 
 namespace zero {
-class Parser {
+
+struct ParseError : public std::runtime_error {
+    ParseError(const Token &token, const std::string &msg)
+        : std::runtime_error{msg}, token{token} {}
+
+    const Token token;
+};
+
+class Parser {  
 public:
     explicit Parser(const std::vector<Token> &tokens) : tokens(tokens){};
-    std::vector<std::unique_ptr<Stmt>> parse();
+    std::vector<std::unique_ptr<Stmt>> parse();   
 
 private:
-    struct ParseError : public std::runtime_error {
-        ParseError(const Token &token, const std::string &msg)
-            : std::runtime_error{msg}, token{token} {}
-
-        const Token token;
-    };
     std::unique_ptr<Expr> expression();
+    std::unique_ptr<Expr> assignment();
+    std::unique_ptr<Expr> equality();
+    std::unique_ptr<Expr> comparison();
+    std::unique_ptr<Expr> term();
+    std::unique_ptr<Expr> factor();
+    std::unique_ptr<Expr> unary();
+    std::unique_ptr<Expr> primary();
+
     std::unique_ptr<Stmt> declaration();
     std::unique_ptr<Stmt> statement();
     std::unique_ptr<Stmt> print_statement();
@@ -31,13 +41,7 @@ private:
     std::unique_ptr<Stmt> for_statement();
     std::unique_ptr<Stmt> while_statement();
     std::vector<std::unique_ptr<Stmt>> block();
-    std::unique_ptr<Expr> assignment();
-    std::unique_ptr<Expr> equality();
-    std::unique_ptr<Expr> comparison();
-    std::unique_ptr<Expr> term();
-    std::unique_ptr<Expr> factor();
-    std::unique_ptr<Expr> unary();
-    std::unique_ptr<Expr> primary();
+
     template <class... T>
     bool match(T... type);
     Token consume(token_type type, const std::string &msg);
@@ -48,6 +52,7 @@ private:
     bool check(token_type type);
     void synchronize();
 
+private:
     const std::vector<Token> &tokens;
     unsigned int current = 0;
 };
